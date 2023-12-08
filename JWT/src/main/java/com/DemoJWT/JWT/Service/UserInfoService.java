fd143c2ccd99e165c7af42d7cf723938bb449bc9
selvapriya748 <151836798+selvapriya748@users.noreply.github.com>
2023-12-08 19:24:54 +0530

    Add files via upload

---
+++ JWT/src/main/java/com/DemoJWT/JWT/Service/UserInfoService.java
@@ -0,0 +1,42 @@
+package com.DemoJWT.JWT.Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.security.core.userdetails.UserDetails; 
+import org.springframework.security.core.userdetails.UserDetailsService; 
+import org.springframework.security.core.userdetails.UsernameNotFoundException; 
+import org.springframework.security.crypto.password.PasswordEncoder; 
+import org.springframework.stereotype.Service;
+
+import com.DemoJWT.JWT.Model.UserInfo;
+import com.DemoJWT.JWT.Repository.UserInfoRepository;
+
+import java.util.Optional; 
+
+@Service
+public class UserInfoService implements UserDetailsService { 
+
+	@Autowired
+	private UserInfoRepository repository; 
+
+	@Autowired
+	private PasswordEncoder encoder; 
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { 
+
+		Optional<UserInfo> userDetail = repository.findByName(username); 
+
+		// Converting userDetail to UserDetails 
+		return userDetail.map(UserInfoDetails::new) 
+				.orElseThrow(() -> new UsernameNotFoundException("User not found " + username)); 
+	} 
+
+	public String addUser(UserInfo userInfo) { 
+		userInfo.setPassword(encoder.encode(userInfo.getPassword())); 
+		repository.save(userInfo); 
+		return "User Added Successfully"; 
+	} 
+
+
+} 
